8853df2be22c40d8933805a796142ab1300b1655
Rob Stoecklein <rstoeck@gmail.com>
2020-02-29 16:46:17 -0600

    Initial commit.

---
+++ src/main/java/com/github/robstoecklein/examples/lombok/DataBeanStandard.java
@@ -0,0 +1,80 @@
+package com.github.robstoecklein.examples.lombok;
+
+import java.util.Objects;
+
+public class DataBeanStandard {
+
+    private boolean isEnabled;
+    private Integer age;
+    private String firstName;
+
+    public DataBeanStandard() {
+    }
+
+    public DataBeanStandard(final boolean isEnabled, final Integer age, final String firstName) {
+        this.isEnabled = isEnabled;
+        this.age = age;
+        this.firstName = firstName;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setIsEnabled(final boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(final Integer age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final DataBeanStandard that = (DataBeanStandard) obj;
+
+        if (isEnabled != that.isEnabled) {
+            return false;
+        }
+        if (!Objects.equals(age, that.age)) {
+            return false;
+        }
+        return Objects.equals(firstName, that.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (isEnabled ? 1 : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DataBeanStandard{" +
+                "isEnabled=" + isEnabled +
+                ", age=" + age +
+                ", firstName='" + firstName + '\'' +
+                '}';
+    }
+}
